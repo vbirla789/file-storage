68b7121c35b092e36f8146bb9954d39440550d1f
vbirla789 <birlavishal90@gmail.com>
2024-06-15 01:03:20 +0530

    toast added using shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -11,44 +11,141 @@ import {
 import { Button } from "@/components/ui/button";
 import { useMutation, useQuery } from "convex/react";
 import { api } from "../convex/_generated/api";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+import { useToast } from "@/components/ui/use-toast";
+
+const formSchema = z.object({
+  title: z.string().min(1).max(200),
+  file: z
+    .custom<FileList>((val) => val instanceof FileList, "Required")
+    .refine((files) => files.length > 0, `Required`),
+});
 
 export default function Home() {
+  const { toast } = useToast();
   const organization = useOrganization();
   const user = useUser();
+  const generateUploadUrl = useMutation(api.files.generateUploadUrl);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: "",
+      file: undefined,
+    },
+  });
+
+  const fileRef = form.register("file");
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    if (!orgId) return;
+    const postUrl = await generateUploadUrl();
+
+    const result = await fetch(postUrl, {
+      method: "POST",
+      headers: { "Content-Type": values.file[0].type },
+      body: values.file[0],
+    });
+
+    const { storageId } = await result.json();
+
+    await createFile({ name: values.title, orgId, fileId: storageId });
+
+    form.reset();
+
+    setIsFileDialogOpen(false);
+
+    toast({
+      variant: "success",
+      title: "File uploaded",
+      description: "Now everyone can view your file!",
+    });
+  }
 
   let orgId: string | undefined = undefined;
   if (organization.isLoaded && user.isLoaded) {
     orgId = organization.organization?.id ?? user.user?.id;
   }
+
+  const [isFileDialogOpen, setIsFileDialogOpen] = useState(false);
   const files = useQuery(api.files.getFiles, orgId ? { orgId } : "skip");
   const createFile = useMutation(api.files.createFile);
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <SignedIn>
-        <SignOutButton>
-          <Button>Sign out</Button>
-        </SignOutButton>
-      </SignedIn>
-      <SignedOut>
-        <SignInButton mode="modal">
-          <Button>Sign in</Button>
-        </SignInButton>
-      </SignedOut>
 
-      {files?.map((file) => {
-        return <div key={file._id}>{file.name}</div>;
-      })}
-      <Button
-        onClick={() => {
-          if (!orgId) return;
-          createFile({
-            name: "hello world",
-            orgId,
-          });
-        }}
-      >
-        Click me
-      </Button>
+  return (
+    <main className="container mx-auto pt-12">
+      <div className="flex justify-between items-center">
+        <h1 className="text-4xl font-bold">Your Files</h1>
+        <Dialog open={isFileDialogOpen} onOpenChange={setIsFileDialogOpen}>
+          <DialogTrigger asChild>
+            <Button>Upload File</Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle className="mb-8">Upload your file</DialogTitle>
+              <DialogDescription>
+                <div>
+                  <Form {...form}>
+                    <form
+                      onSubmit={form.handleSubmit(onSubmit)}
+                      className="space-y-8"
+                    >
+                      <FormField
+                        control={form.control}
+                        name="title"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormLabel>Title</FormLabel>
+                            <FormControl>
+                              <Input {...field} />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                      <FormField
+                        control={form.control}
+                        name="file"
+                        render={({ field: { onChange }, ...field }) => (
+                          <FormItem>
+                            <FormLabel>File</FormLabel>
+                            <FormControl>
+                              <Input type="file" {...fileRef} />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                      <Button type="submit">Submit</Button>
+                    </form>
+                  </Form>
+                </div>
+              </DialogDescription>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
+      </div>
+
+      {files?.map((file) => <div key={file._id}>{file.name}</div>)}
     </main>
   );
 }
--- convex/files.ts
+++ convex/files.ts
@@ -2,6 +2,16 @@ import { mutation, MutationCtx, query, QueryCtx } from "./_generated/server";
 import { ConvexError, v } from "convex/values";
 import { getUser } from "./users";
 
+export const generateUploadUrl = mutation(async (ctx) => {
+  const identity = await ctx.auth.getUserIdentity();
+
+  if (!identity) {
+    throw new ConvexError("You must be signed in to create a file");
+  }
+
+  return await ctx.storage.generateUploadUrl();
+});
+
 async function hasAccessToOrg(
   ctx: QueryCtx | MutationCtx,
   tokenIdentifier: string,
@@ -18,12 +28,11 @@ async function hasAccessToOrg(
 export const createFile = mutation({
   args: {
     name: v.string(),
+    fileId: v.id("_storage"),
     orgId: v.string(),
   },
   async handler(ctx, args) {
     const identity = await ctx.auth.getUserIdentity();
-
-    // console.log(identity);
 
     if (!identity) {
       throw new ConvexError("You must be signed in to create a file");
@@ -44,6 +53,7 @@ export const createFile = mutation({
     await ctx.db.insert("files", {
       name: args.name,
       orgId: args.orgId,
+      fileId: args.fileId,
     });
   },
 });
