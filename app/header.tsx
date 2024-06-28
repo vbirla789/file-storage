4b6a0a5b70b73607141304a5831fb13e436e17f3
vbirla789 <birlavishal90@gmail.com>
2024-06-28 14:41:20 +0530

    adding landing page

--- app/header.tsx
+++ app/header.tsx
@@ -6,18 +6,24 @@ import {
   SignInButton,
   UserButton,
   UserProfile,
+  useSession,
 } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 
 export function Header() {
   return (
-    <div className="border-b py-4 bg-gray-50">
+    <div className="relative z-10 border-b py-4 bg-gray-50">
       <div className="container mx-auto justify-between flex items-center">
         <Link href="/" className="flex gap-2 items-center">
           <Image src="/logo.png" alt="Logo" width={40} height={40} />
           File Drive
         </Link>
+        {/* <SignedIn>
+        <Button variant={"outline"}>
+            <Link href="/dashboard/files">Your Files</Link>
+          </Button>
+        </SignedIn> */}
         <Button variant={"outline"}>
           <Link href="/dashboard/files">Your Files</Link>
         </Button>
